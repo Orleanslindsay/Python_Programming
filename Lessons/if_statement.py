8f670e41fa9915bac4892e37d89c495df32f280c
Orleanslindsay <88582316+Orleanslindsay@users.noreply.github.com>
2021-08-27 23:50:57 -0700

    Update if_statement.py

--- Lessons/if_statement.py
+++ Lessons/if_statement.py
@@ -1,4 +1,10 @@
 won_bet = True 
 big_win = True
 if won_bet and  big_win:
-	print("You caan now stop betting!")
+	print("You can now stop betting!")
+
+	#Using the or operator
+	won_bet = True
+	big_win = True
+	if won_bet or big_win:
+		print("You can now stop betting!")
