77e7934aacb260041f39341be59f545b22a6930d
Orleanslindsay <88582316+Orleanslindsay@users.noreply.github.com>
2021-09-04 01:42:41 -0700

    if_elif_else_statement

---
+++ Lessons/if_and_else_statemennt.py
@@ -0,0 +1,6 @@
+integer_number = int(input("Enter an integer number: "))
+
+if integer_number % 2 == 0:
+	print(integer_number, "is an even number.")
+else:
+	print(integer_number," is an odd number.")
--- Lessons/if_statement.py
+++ Lessons/if_statement.py
@@ -5,6 +5,7 @@ if won_bet and  big_win:
 
 	#Using the or operator
 	won_bet = True
-	big_win = True
+	big_win = False
 	if won_bet or big_win:
 		print("You can now stop betting!")
+		 
