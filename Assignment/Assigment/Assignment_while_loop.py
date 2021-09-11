e2f4240a037f12528b118cf9ed1b694a84df9bbe
Orleanslindsay <88582316+Orleanslindsay@users.noreply.github.com>
2021-09-11 03:46:21 -0700

    Assignment

--- Assignment/Assigment/Assignment_while_loop.py
+++ Assignment/Assigment/Assignment_while_loop.py
@@ -1,13 +1,16 @@
 while True:
 	input_value = input("Enter student's score:")
-	student_score = float(input_value)
+	if input_value.capitalize() == "Exit":
+		break
+	elif input_value.capitalize() != "Exit" and input_value.isnumeric() == False:
+		print("Invalid Entry!")
+	else:
+		student_score = float(input_value)
 	if student_score >= 90 and student_score <=100:
 		print("Awarded a laptop!")
 	elif student_score >=60 and student_score <= 90:
 		print("Awarded a tablet!")
 	elif student_score < 60 and student_score > 0:
 		print("Awarded Nothing")
-	while(input_value).capitalize() == "Exit":
-		break
 	else:
 		print("score out of range")
--- Assignment/Assigment/home.py
+++
@@ -1,6 +0,0 @@
-student_score = int(input("ENTER A NUMBER:"))
-score = ["90","91","92","93",]
-for student_score in score:
-	print(student_score) 
-	if student_score == score:
-		print("wins an award")
